7e83a89d2553929dfaff3fe66e5bdaab6f61729f
David Johnston <david@blacksheepcode.com>
2019-05-25 12:33:04 +1000

    Add artist filter

--- src/App.js
+++ src/App.js
@@ -1,14 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import data from "./data/data.json";
-console.log(data);
+import rawdata from "./data/data.json";
 function App() {
+
+  const [data, updateData] = useState(rawdata);
+  const [filter, updateFilter] = useState('');
+
+
+  const artists = data.reduce((acc, cur) => {
+
+    const currentArray = acc[cur.ARTIST] || [];
+    return {
+      ...acc,
+      [cur.ARTIST]: [...currentArray, cur]
+    }
+  }, {});
+
+  console.log(artists);
+
+
+  const displayData = (filter === '') ? data : data.filter(v => v.ARTIST.toUpperCase().startsWith(filter.toUpperCase()))
   return (
     <div className="App">
+      {filter}
+      <div>
+        <input
+          value={filter}
+          placeholder="Artist"
+          onChange={e => updateFilter(e.target.value)} />
+      </div>
+
+
+      <ul>
+
+        {Object.keys(artists).map(str =>
+          <li><a href="#" onClick={() => updateFilter(str)}> {str}</a></li>
+        )}
+      </ul>
       <table>
         <tbody>
-          {data.map(row => <tr>
+          {displayData.map(row => <tr>
 
             {Object.values(row).map(item => <td>
               {item}
